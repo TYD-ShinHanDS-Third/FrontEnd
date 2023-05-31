a2748c7cfdd6d38c361a36d93ecab72bb4abe66b
YYJ-1229 <90436@naver.com>
2023-05-31 11:46:54 +0900

    [feat] : 공고조회 게시판 디자인 수정

--- src/page/pan/PanList.js
+++ src/page/pan/PanList.js
@@ -1,16 +1,8 @@
-import React, { useEffect } from "react";
-import "../../css/PanList.css";
-
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Checkbox from "@mui/material/Checkbox";
-import Paper from "@mui/material/Paper";
-import { AccessAlarm, ThreeDRotation } from "@mui/icons-material";
-import StarIcon from "@mui/icons-material/Star";
+import React from "react";
+import "../../css/pan/PanList.css";
+import { Link, Route, Routes } from "react-router-dom";
+import PanDetail from "./PanDetail";
+import NoticeList from "./NoticeList";
 
 function PanList(props) {
   return (
@@ -33,49 +25,11 @@ function PanList(props) {
           <li>서울</li>
         </ul>
       </div>
-      <div className="noticelist">
-        <label class="fav">
-          <Checkbox defaultChecked color="success" />
-          관심있는 공고만 보기
-        </label>
-
-        <label class="recruiting">
-          <Checkbox defaultChecked color="success" />
-          모집중인 공고만 보기
-        </label>
-
-        <hr className="line" />
-
-        <TableContainer className="noticeTable">
-          <table sx={{ minWidth: 700 }} size="small" aria-label="a dense table">
-            <TableHead>
-              <TableRow>
-                <TableCell>공고 번호</TableCell>
-                <TableCell>공고 이름</TableCell>
-                <TableCell>지역</TableCell>
-                <TableCell>공고 게시일</TableCell>
-                <TableCell>공고 마감일</TableCell>
-                <TableCell>공고 상태</TableCell>
-                <TableCell></TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell>1</TableCell>
-                <TableCell>이름입니당</TableCell>
-                <TableCell>서울</TableCell>
-                <TableCell>2023.12.01</TableCell>
-                <TableCell>2023.12.18</TableCell>
-                <TableCell>공고중</TableCell>
-                <TableCell>
-                  <StarIcon />
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </table>
-        </TableContainer>
+      <div className="noticearea">
+        <Routes>
+          <Route path="detail" element={<PanDetail />}></Route>
+          <Route path="/" element={<NoticeList />}></Route>
+        </Routes>
       </div>
     </div>
   );
