385d4b739eff0607758a379e27d130dde0851945
seongg <seongeun7877@naver.com>
2023-06-01 14:23:00 +0900

    [feat] : 그리드 레이아웃 및 FullCalendar 이용하여 달력 구현

--- src/page/my/MyPage.js
+++ src/page/my/MyPage.js
@@ -1,9 +1,48 @@
 import React from "react";
-
+import "../../css/my/MyPage.css";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
 function MyPage(props) {
   return (
-    <div>
-      <h1>마이 페이지</h1>
+    <div className="myPage">
+      <div className="myContainer">
+        <div className="calendar">
+          <FullCalendar
+            defaultView="dayGridMonth"
+            plugins={[dayGridPlugin]}
+            height={"auto"}
+            events={[
+              {
+                title: "event 1",
+                start: "2023-06-13",
+                end: "2023-06-16",
+                backgroundColor: "#031389",
+                borderColor: "#031389",
+                textColor: "white",
+              },
+              {
+                title: "event 2",
+                start: "2023-06-22",
+                end: "2023-06-24",
+                backgroundColor: "#FFFEDD",
+                borderColor: "#FFFEDD",
+                textColor: "black",
+              },
+              {
+                title: "event 3",
+                start: "2023-06-26",
+                end: "2023-06-28",
+                backgroundColor: "#609966",
+                borderColor: "#609966",
+                textColor: "white",
+              },
+            ]}
+          />
+        </div>
+        <div className="myLoan">내 대출 목록</div>
+        <div className="myAcc">내 연결 계좌</div>
+        <div className="myEdit">회원정보 수정</div>
+      </div>
     </div>
   );
 }
