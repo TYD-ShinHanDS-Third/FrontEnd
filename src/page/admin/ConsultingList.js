76d1424dc5fd5b4a96b8f348e6d1a7b5666cbeee
YYJ-1229 <90436@naver.com>
2023-06-07 09:20:25 +0900

    [fix] : 사용하지 않는 페이지 import 제거

--- src/page/admin/ConsultingList.js
+++ src/page/admin/ConsultingList.js
@@ -1,6 +1,4 @@
 import React from "react";
-import "../../css/admin/ConsultingList.css";
-
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
