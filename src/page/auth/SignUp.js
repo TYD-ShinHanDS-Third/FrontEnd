ee9dd71bec7d22ba5950cc43da4982e7b5af6279
seongg <seongeun7877@naver.com>
2023-06-07 09:10:35 +0900

    [style] : 배경색 변경

--- src/page/auth/SignUp.js
+++ src/page/auth/SignUp.js
@@ -4,6 +4,7 @@ import SignUpForm from "./SignUpForm";
 import Login from "./Login";
 import { Route, Routes } from "react-router-dom";
 function SignUp(props) {
+  document.body.style.backgroundColor = "#f5f5f5";
   return (
     <div className="entire">
       <div className="circle"></div>
