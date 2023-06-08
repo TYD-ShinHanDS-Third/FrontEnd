e967448e75fdeac55289dd9332a5c9564f234cb8
YYJ-1229 <90436@naver.com>
2023-06-08 11:45:50 +0900

    [feat] : 지역 불러오기 수정중

--- src/page/pan/PanList.js
+++ src/page/pan/PanList.js
@@ -1,28 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../css/pan/PanList.css";
 import { Link, Route, Routes } from "react-router-dom";
 import PanDetail from "./PanDetail";
 import NoticeList from "./NoticeList";
+import axios from "axios";
 
 function PanList(props) {
+  const [panlist, setPanlist] = useState([]);
+  const [panlocation, setPanlocation] = useState([]);
+
+  const pan = useState({
+    panId: 0,
+    panName: "",
+    location: "",
+    panStartDate: "",
+    panEndDate: "",
+    panState: "",
+    favorite: false
+  });
+
+  async function getList(list) {
+    const listurl = "http://localhost:3000/data/pan/panlistData.json";
+    await axios
+      .get(listurl)
+      .then(function (response) {
+        console.log(response);
+        setPanlist(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    const locationurl = "http://localhost:3000/data/pan/panlocationData.json";
+    await axios
+      .get(locationurl)
+      .then(function (response) {
+        console.log(response);
+        setPanlocation(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    getList();
+    setLocation(panlocation);
+  }, []);
+
+  function setLocation(panlocation) {
+    const locations = [];
+    for (let item in Object.keys(panlocation)) {
+      locations.push(panlocation[item].location);
+    }
+  }
+
   return (
     <div className="panlist">
       <div className="region_content">
         <ul className="region_list">
-          <li>서울</li>
-          <li>경기도</li>
-          <li>서울</li>
-          <li>서울</li>
-          <li>서울</li>
-          <li>서울</li>
-          <li>서울</li>
-          <li>경기도</li>
-          <li>서울</li>
-          <li>서울</li>
-          <li>경기도</li>
-          <li>서울</li>
-          <li>서울</li>
-          <li>서울</li>
+          <li>{panlocation}</li>
         </ul>
       </div>
 
