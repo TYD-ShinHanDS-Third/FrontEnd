3e4047584758eb5e51f6470912120fefbaf6f598
Your Name <90436@naver.com>
2023-06-13 17:37:32 +0900

    [feat] : 관리자 - 회원 상담방 리스트

--- src/page/admin/ConsultingList.js
+++ src/page/admin/ConsultingList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -6,8 +6,47 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import { Link } from "react-router-dom";
+import { Cookies } from "react-cookie";
+import axios from "axios";
 
-function ConsultingList(props) {
+function ConsultingList() {
+  const [userConList, setUserConsultList] = useState([]);
+  const [pageTotal, setPageTotal] = useState(0);
+  const [pageNum, setPageNum] = useState(0);
+
+  useEffect(() => {
+    userCList();
+  }, []);
+
+  //회원 상담  - 조회
+  async function userCList() {
+    const cookies = new Cookies();
+    const token = cookies.get("jwtToken");
+
+    const listurl = "/hows/admin/consult";
+
+    await axios
+      .get(listurl, {
+        params: {
+          page: pageNum,
+          size: "9",
+        },
+        headers: {
+          "Content-type": "application/json",
+          token: token,
+        },
+      })
+      .then(function (response) {
+        setUserConsultList(response.data);
+        setPageTotal(response.data.total);
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  console.log(userConList);
   return (
     <div className="manageuser">
       <TableContainer className="man_userlist">
@@ -28,35 +67,31 @@ function ConsultingList(props) {
           </TableHead>
 
           <TableBody>
-            <TableRow
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell>회원 아이디123</TableCell>
-              <TableCell>회원 이름123</TableCell>
-              <TableCell>대출 신청 상품123</TableCell>
-              <TableCell>대출 신청 상태</TableCell>
-              <Link
-                to="/hows/admin/consult/chatroom"
-                style={{ textDecoration: "none" }}
-              >
-                <TableCell style={{ color: "green" }}>상담방 입장</TableCell>
-              </Link>
-            </TableRow>
-
-            <TableRow
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell>회원 아이디123</TableCell>
-              <TableCell>회원 이름123</TableCell>
-              <TableCell>대출 신청 상품123</TableCell>
-              <TableCell>대출 신청 상태</TableCell>
-              <Link
-                to="/hows/admin/consult/chatroom"
-                style={{ textDecoration: "none" }}
-              >
-                <TableCell style={{ color: "green" }}>상담방 입장</TableCell>
-              </Link>
-            </TableRow>
+            {userConList.map((item, index) => {
+              return (
+                <TableRow
+                  key={item.memberid.memberid}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell name="memberid">
+                    {item.memberid.memberid}
+                  </TableCell>
+                  <TableCell name="membername">{item.membername}</TableCell>
+                  <TableCell name="loanname">
+                    {item.loanname.loanname}
+                  </TableCell>
+                  <TableCell name="loanstate">{item.loanstate}</TableCell>
+                  <Link
+                    to="/hows/admin/consult/chatroom"
+                    style={{ textDecoration: "none" }}
+                  >
+                    <TableCell style={{ color: "green" }}>
+                      상담방 입장
+                    </TableCell>
+                  </Link>
+                </TableRow>
+              );
+            })}
           </TableBody>
         </Table>
       </TableContainer>
--- src/page/admin/ManPage.js
+++ src/page/admin/ManPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Route, Routes } from "react-router-dom";
 import ManageUser from "./ManageUser";
@@ -6,12 +6,113 @@ import ConsultingList from "./ConsultingList";
 import ManageDocs from "./ManageDocs";
 import ManageApplyLoan from "./ManageApplyLoan";
 import Consulting from "./Consulting";
+import axios from "axios";
+import { Cookies } from "react-cookie";
 
 function ManPage(props) {
   document.body.style.backgroundColor = "#9DC08B";
+  const [userConList, setUserConsultList] = useState([]);
+  const [userList, setUserList] = useState([]);
+  const [pageTotal, setPageTotal] = useState(0);
+  const [pageNum, setPageNum] = useState(0);
+
+  // useEffect(() => {
+  //   getUserList();
+  // }, []);
+
+  // useEffect(() => {
+  //   userCList();
+  // }, []);
+
+  //사용지 확인 - get
+  async function getUserList() {
+    const cookies = new Cookies();
+    const token = cookies.get("jwtToken");
+
+    const listurl = "/hows/admin/user";
+
+    await axios
+      .get(listurl, {
+        params: {
+          page: pageNum,
+          size: "9",
+        },
+        headers: {
+          "Content-type": "application/json",
+          token: token,
+        },
+      })
+      .then(function (response) {
+        setUserList(response.data.obj);
+        setPageTotal(response.data.total);
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  //사용지 확인 - put
+  async function putUserList(userid, userrole) {
+    const cookies = new Cookies();
+    const token = cookies.get("jwtToken");
+
+    const listurl = "/hows/admin/user";
+
+    await axios
+      .put(
+        listurl,
+        { userid: userid, userrole: userrole },
+        {
+          headers: {
+            "Content-type": "application/json",
+            token: token,
+          },
+        }
+      )
+      .then(function (response) {
+        setUserConsultList(response.data.obj);
+        setPageTotal(response.data.total);
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  //회원 상담  - 조회
+  async function userCList() {
+    const cookies = new Cookies();
+    const token = cookies.get("jwtToken");
+
+    const listurl = "/hows/admin/consult";
+
+    await axios
+      .get(listurl, {
+        params: {
+          page: pageNum,
+          size: "9",
+        },
+        headers: {
+          "Content-type": "application/json",
+          token: token,
+        },
+      })
+      .then(function (response) {
+        setUserConsultList(response.data);
+        setPageTotal(response.data.total);
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   return (
     <Routes>
-      <Route path="user" element={<ManageUser />}></Route>
+      <Route
+        path="user"
+        element={<ManageUser userList={userList} putUserList={putUserList} />}
+      ></Route>
       <Route path="consult" element={<ConsultingList />}></Route>
       <Route path="form" element={<ManageApplyLoan />}></Route>
       <Route path="form/checklist" element={<ManageDocs />}></Route>
