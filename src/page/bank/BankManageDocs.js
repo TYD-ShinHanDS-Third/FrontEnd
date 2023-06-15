02223bbefe7ae138d1b4eb377602dc28582c5e9e
Your Name <90436@naver.com>
2023-06-15 18:44:32 +0900

    [style] : bank 페이지 디자인 수정

--- src/page/bank/BankManageDocs.js
+++ src/page/bank/BankManageDocs.js
@@ -1,42 +1,80 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 function BankManageDocs(props) {
+  const location = useLocation();
+  console.log(location.state);
+  const membername = location.state.membername;
+  const loanname = location.state.loanname;
   return (
     <div className="managedocs">
       <div className="managedocs_header">
         <div className="managedocs_title">
-          <h2>버팀목 전세자금 대출 - 회원 이름</h2>
+          <h2>
+            {loanname} - {membername}
+          </h2>
         </div>
+        <hr className="loanhr" />
       </div>
       <div className="managedocs_body">
-        <hr className="loanhr" />
         <div className="managedocs_uplods">
           <div className="files">
             <div className="file1 file">
-              <h2>주민등록 등본</h2>
-              <button className="upload_btn">확인</button>
+              <h2 style={{ backgroundColor: "#A4BC92" }}>주민등록 등본</h2>
+              <button
+                className="upload_btn"
+                style={{ backgroundColor: "#EAE7B1", color: "black" }}
+              >
+                확인
+              </button>
             </div>
             <div className="file2 file">
-              <h2>확정 일자부 임대차 계약서</h2>
-              <button className="upload_btn">확인</button>
+              <h2 style={{ backgroundColor: "#A4BC92" }}>
+                확정 일자부 임대차 계약서
+              </h2>
+              <button
+                className="upload_btn"
+                style={{ backgroundColor: "#EAE7B1", color: "black" }}
+              >
+                확인
+              </button>
             </div>
             <div className="file3 file">
-              <h2>임차주택 건물 등기부등본</h2>
-              <button className="upload_btn">확인</button>
+              <h2 style={{ backgroundColor: "#A4BC92" }}>
+                임차주택 건물 등기부등본
+              </h2>
+              <button
+                className="upload_btn"
+                style={{ backgroundColor: "#EAE7B1", color: "black" }}
+              >
+                확인
+              </button>
             </div>
             <div className="file4 file">
-              <h2>결혼예정 증빙 서류</h2>
-              <button className="upload_btn">확인</button>
+              <h2 style={{ backgroundColor: "#A4BC92" }}>결혼예정 증빙 서류</h2>
+              <button
+                className="upload_btn"
+                style={{ backgroundColor: "#EAE7B1", color: "black" }}
+              >
+                확인
+              </button>
             </div>
             <div className="file5 file">
-              <h2>근로자 | 건강보험 자격 실득 확인서</h2>
-              <button className="upload_btn">확인</button>
+              <h2 style={{ backgroundColor: "#A4BC92" }}>
+                근로자 | 건강보험 자격 실득 확인서
+              </h2>
+              <button
+                className="upload_btn"
+                style={{ backgroundColor: "#EAE7B1", color: "black" }}
+              >
+                확인
+              </button>
             </div>
           </div>
           <div className="loanapplybtn">
             <button
               className="finalapply_btn"
-              style={{ backgroundColor: "#55967e" }}
+              style={{ backgroundColor: "#285430", color: "white" }}
             >
               신청하기
             </button>
--- src/page/bank/BankMenu.js
+++ src/page/bank/BankMenu.js
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Cookies } from "react-cookie";
 import { Link } from "react-router-dom";
 
 function BankMenu(props) {
+  const [token, setToken] = useState(undefined);
+
+  const signout = (e) => {
+    const cookies = new Cookies();
+    cookies.remove("jwtToken");
+    setToken(undefined);
+    window.location.href = "/hows";
+  };
+
+  useEffect(() => {
+    const cookies = new Cookies();
+    setToken(cookies.get("jwtToken"));
+  }, []);
   return (
     <div className="menubar">
-      <Link
-        to="/hows/bank/loanlist"
-        style={{ textDecoration: "none", color: "white" }}
-      >
-        <h3 className="menu">대출신청 확인</h3>
-      </Link>
+      <div className="menutab">
+        <Link
+          to="/hows/bank/loanlist"
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          <h3 className="menu">대출신청 확인</h3>
+        </Link>
+        <div className="menubtn">
+          <Link
+            to="/hows/auth/login"
+            style={{
+              textDecoration: "none",
+              display: token === undefined ? "block" : "none",
+            }}
+          >
+            <button className="menu">로그인</button>
+          </Link>
+          <div style={{ display: token === undefined ? "none" : "block" }}>
+            <button className="menu" onClick={signout}>
+              로그아웃
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
