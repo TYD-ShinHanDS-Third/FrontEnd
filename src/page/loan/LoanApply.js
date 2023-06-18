d70a6567ddffad27768ea99312af6823423dbdfc
YYJ-1229 <90436@naver.com>
2023-06-18 18:30:30 +0900

    [feat] : 화상통화 (내 화면 + 소리만 연결)

--- src/page/loan/LoanApply.js
+++ src/page/loan/LoanApply.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../css/loan/LoanApply.css";
 import { Link } from "react-router-dom";
+import Maincam from "./Maincam";
 
 function LoanApply(props) {
   return (
@@ -9,30 +10,7 @@ function LoanApply(props) {
         <h1>여기 상품 설명</h1>
       </div>
       <div className="loanapply_chat">
-        <div className="chat_context">
-          <div className="you">
-            <h3>상담을 시작합니다.</h3>
-            <h3>상담을 시작.</h3>
-          </div>
-          <div className="me">
-            <h3>안녕하세요!!</h3>
-            <h3>!!sdhfjshk</h3>
-            <h3>!!sdffsd</h3>
-          </div>
-        </div>
-
-        <hr style={{ marginBottom: "0" }} />
-        <div className="chat_input">
-          <textarea
-            className="input_context"
-            style={{
-              width: "100%",
-              height: "90%",
-              textAlign: "left",
-            }}
-          />
-          <button className="send">전송</button>
-        </div>
+        <Maincam />
       </div>
 
       <Link to="/hows/loan/detail/limit/uploaddocs">
