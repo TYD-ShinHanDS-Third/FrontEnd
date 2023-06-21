97f4418773f799633e36c5e9b83ea21b38be5dcf
seongg <seongeun7877@naver.com>
2023-06-21 20:03:20 +0900

    [feat] : 은행원 대출 승인/거절 연결 완료

--- src/page/bank/BankManageDocs.js
+++ src/page/bank/BankManageDocs.js
@@ -1,11 +1,93 @@
+import axios from "axios";
 import React from "react";
+import { confirmAlert } from "react-confirm-alert";
 import { useLocation } from "react-router-dom";
 
 function BankManageDocs(props) {
   const location = useLocation();
   console.log(location.state);
   const membername = location.state.membername;
+  const memberphone = location.state.tel.replaceAll("-", "");
   const loanname = location.state.loanname;
+  const loanid = location.state.loanid;
+
+  //승인 및 거절
+  const apply = () => {
+    const url = "http://192.168.0.55:8888/hows/bank/approval";
+    axios
+      .put(url, null, {
+        headers: {
+          "Content-Type": `application/json`,
+        },
+        params: {
+          memloanid: loanid,
+          membername: membername,
+          tel: memberphone,
+          loanname: loanname,
+        },
+      })
+      .then((res) => {
+        if (res.data === "success") {
+          console.log("update success");
+          confirmAlert({
+            title: "승인 완료",
+            buttons: [
+              {
+                label: "확인",
+                style: { backgroundColor: "#9db2bf" },
+                onClick: () => {},
+              },
+            ],
+          });
+        } else {
+          console.log("update fail");
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(() => {
+        console.log("request end");
+      });
+  };
+  const refuse = () => {
+    const url = "http://192.168.0.55:8888/hows/bank/refusal";
+    axios
+      .put(url, null, {
+        headers: {
+          "Content-Type": `application/json`,
+        },
+        params: {
+          memloanid: loanid,
+          membername: membername,
+          tel: memberphone,
+          loanname: loanname,
+        },
+      })
+      .then((res) => {
+        if (res.data === "success") {
+          console.log("update success");
+          confirmAlert({
+            title: "거절 완료",
+            buttons: [
+              {
+                label: "확인",
+                style: { backgroundColor: "#9db2bf" },
+                onClick: () => {},
+              },
+            ],
+          });
+        } else {
+          console.log("update fail");
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(() => {
+        console.log("request end");
+      });
+  };
   return (
     <div className="managedocs">
       <div className="managedocs_header">
@@ -75,8 +157,16 @@ function BankManageDocs(props) {
             <button
               className="finalapply_btn"
               style={{ backgroundColor: "#285430", color: "white" }}
+              onClick={apply}
             >
-              신청하기
+              승인하기
+            </button>
+            <button
+              className="finalapply_btn"
+              style={{ backgroundColor: "#285430", color: "white" }}
+              onClick={refuse}
+            >
+              거절하기
             </button>
           </div>
         </div>
