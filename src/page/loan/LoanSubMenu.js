c06485ee81813ebfabdcd090cf549eb0364c8605
seongg <seongeun7877@naver.com>
2023-06-27 16:24:28 +0900

    [fix] : 대출 서브 메뉴 선택 시 색상 변경

--- src/page/loan/LoanSubMenu.js
+++ src/page/loan/LoanSubMenu.js
@@ -4,11 +4,26 @@ import "../../css/pan/PanSubMenu.css";
 const banklist = ["전체", "신한", "국민", "우리", "하나"];
 
 function LoanSubMenu({ findBank }) {
+  const changeColor = (loc) => {
+    for (let i = 0; i < banklist.length; i++) {
+      var reset = banklist[i];
+      document.getElementById(reset).style.color = "black";
+    }
+    document.getElementById(loc).style.color = "#2c7929";
+  };
   return (
     <div className="pansubmenu">
       {banklist.map((item, index) => {
         return (
-          <li className="loclist" key={item} onClick={() => findBank({ item })}>
+          <li
+            className="loclist"
+            id={item}
+            key={item}
+            onClick={() => {
+              findBank({ item });
+              changeColor(item);
+            }}
+          >
             {item}
           </li>
         );
